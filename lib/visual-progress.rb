cb35a467aa1d41a60424be148c3754abbfd13872
Eric Jacobs <ejacobs@instructure.com>
2017-10-25 16:35:31 -0600

    so much

--- lib/visual-progress.rb
+++ lib/visual-progress.rb
@@ -2,163 +2,134 @@
 #2.4.2
 require 'colorize'
 require 'set'
-require 'pry'
+require 'pry-byebug'
 
 module VP
+  class VPTypeError < StandardError; end
+  class NoRoomLeftAtTheInn < StandardError; end
+
   class VisualProgress
     attr_reader :chars, :width
     def initialize(chars: nil)
-      @chars = chars || %w{\\ | / -}
+      #@chars = chars || 
       @line = Line.new
       #Signal.trap('SIGWINCH', resize)
     end
 
     def timestamp
+      spin = %w{\\ | / -}
+      spin_idx = 0
+      a = line.add_component(content: Time.now, left_pad: 4, right_pad: 4)
+      #  byebug
+      b = line.add_component(content: Time.now, justify: :right, left_pad: 4, right_pad: 4)
+      #  byebug
+      c = line.add_component(content: 'hello', style: {background: :blue, color: :red, mode: :bold})
+      #  byebug
+      d = line.add_component(content: spin[0], left_pad: 2, justify: :right, right_pad: 2, style: {background: :yellow, color: :black, mode: :bold})
+      #  byebug
       while true
-        sleep(0.2)
-        write(Time.now)
-      end
-    end
-
-    def write(content, offset = 0)
-      changes = []
-
-      content.to_s.chars.each_with_index do |c, i|
-        candidate = Character.new(content: c)
-        if line.line[i + offset] != candidate
-          line.line[i + offset] = candidate
-          changes << i + offset
-        end
-      end
-
-      if changes.length > 0
-        line.reflow(from: changes[0], to: changes[-1])
-      end
-    end
-
-    def spin
-      first = true
-      while true
-        bg = String.colors.sample
-        chars.each do |ch|
-          sleep 0.2
-          if first
-            first = false
-          else
-            print "\b"
-          end
-          print ch.colorize(color: String.colors.sample, background: bg)
-        end
+        sleep(0.1)
+        a.update(Time.now)
+        b.update(Time.now)
+        d.update(spin[spin_idx % 4])
+        spin_idx += 1
       end
     end
     private
     attr_reader :line
   end
 
-  class Line
-    attr_reader :line
+  class Line < Array
     def initialize(common: nil)
-      @line = []
-      @cursor = 0
-
-      setup_line(common)
+      super()
+      @cursor = Cursor.new(self)
+      @components = {fills: [], fixed: []}
+      @offsets = {}
+      rewiden #TODO trap sigwinch and call this
     end
 
-    def reflow(from: 0, to: nil) # args default to upper and lower bounds
-      # variable prep
-      to.nil? && to = line.length - 1
-
-      # inverted range (we handle right to left scanning automatically)
-      if from > to
-        tmp = from
-        from = to
-        to = tmp
-      end
-
-      # shortcut cases -- entire range can be covered in a single movement
-      if to == from && from == cursor
-        sync_under
-        return
-      end
-
-      if cursor <= from
-        move_cursor_to(to)
-        return
-      end
-
-      if cursor >= to
-        move_cursor_to(from)
-        return
-      end
-
-      # cursor is in the middle of the range, so some places will be double
-      # checked. minimize double checking by moving to the closest bound
-      if (cursor - from).abs < (cursor - to).abs
-        # cursor is closer to start of range
-        move_cursor_to(from)
-        move_cursor_to(to)
-      else
-        # cursor is closer to end of range
-        move_cursor_to(to)
-        move_cursor_to(from)
-      end
+    def refresh(component)
+      cursor.reflow(from: offsets[component], to: offsets[component] + component.length)
     end
 
-    def write(content:, offset: 0)
-      content.chars.each_with_index do |char, idx|
-        line[offset + idx].content = char
+    def reset # TODO refactor bc this is ugly af
+      # populate the line:
+      # 1. clear the line
+      # 2. left and right pinned justifieds are placed in the line
+      # 3. all fills get an equal slice of the remaining space on the line
+      #   # when resizes happen fills extend or retract
+
+      if self.length > 0
+        self[0, self.length] = Array.new(self.length) {Character.new(is_bg: true)} # set all to background
       end
-      reflow
+
+      # Fixed width setting
+      components[:fixed].select {|a| a.justify == :left}.each do |fixed|
+        offset = first_free
+        offsets[fixed] = offset
+        self[offset, fixed.length] = fixed
+      end
+      components[:fixed].select {|a| a.justify == :right}.each do |fixed|
+        offset = first_free(from: :right) - fixed.length
+        offsets[fixed] = offset
+        self[offset, fixed.length] = fixed
+      end
+
+      # Variable width setting (aka fills)
+      if components[:fills].length > 0
+        #byebug
+        fill_space_per = ((first_free(from: :right) - first_free) / components[:fills].length).floor
+      end
+      components[:fills].select {|a| a.justify == :left}.each do |fill|
+        offset = first_free
+        offsets[fill] = offset
+        self[offset, fill_space_per] = fill
+      end
+      components[:fills].select {|a| a.justify == :right}.each do |fill|
+        offset = first_free(from: :right) - fill_space_per
+        offsets[fill] = offset
+        self[first_free(from: :right) - fill_space_per, fill_space_per] = fill
+      end
+      cursor.reflow
+    end
+
+    def add_component(content:, style: {}, fill: false, left_pad: 0, right_pad: 0, justify: :left)
+      args = {content: content, style: style, fill: fill, left_pad: left_pad, right_pad: right_pad, justify: justify}
+      new_component = Component.new(line: self, **args)
+      new_component.fill ? components[:fills].push(new_component) : components[:fixed].push(new_component)
+      reset
+      new_component
+    end
+
+    def del_component(component)
+      # TODO
     end
 
     private
-    attr_accessor :cursor
-    def setup_line(common)
-      terminal_width.times {line << Character.new(from: common)}
-      reflow
+    attr_accessor :cursor, :background, :components, :offsets
+    def first_free(from: :left)
+      idx = nil
+      from == :left && idx = self.index {|x| x.is_bg}
+      from == :right && idx = self.rindex {|x| x.is_bg}
+
+      idx.nil? && raise(VP::NoRoomLeftAtTheInn, 'The line\'s full')
+      idx
     end
 
-    def draw(position)
-      move_cursor_to(position)
-      line[position].draw
-      @cursor += 1
-      off_the_end_check
+    def rewiden
+      delta = terminal_width - self.length
+      delta.negative? ? shorten(delta.abs) : lengthen(delta)
     end
 
-    def off_the_end_check
-      cursor_left if line[cursor].nil?
+    def lengthen(delta)
+      self.push(*Array.new(delta) {Character.new(is_bg: true)})
+      # ^ we do it this janky way because you can't += on self (no self reassign)
+      reset
     end
 
-    def move_cursor_to(desired_location)
-      distance = desired_location - cursor
-      return if distance == 0
-
-      if distance.negative?
-        distance.abs.times {cursor_left}
-      else
-        distance.times {cursor_right}
-      end
-    end
-
-    def sync_under
-      if ! line[cursor].drawn
-        draw(cursor)
-        cursor_left
-      end
-    end
-
-    def cursor_left
-      print "\e[D"
-      @cursor = cursor <= 0 ? 0 : cursor - 1
-
-      sync_under
-    end
-
-    def cursor_right
-      print "\e[C"
-      @cursor = cursor >= line.length - 1 ? line.length - 1 : cursor + 1
-
-      sync_under
+    def shorten(delta)
+      self.pop(delta)
+      reset
     end
 
     def terminal_width
@@ -166,80 +137,136 @@ module VP
     end
   end
 
-  class Character
-    attr_reader :content, :fg_color, :bg_color, :style, :drawn
-    def initialize(from: nil, content: ' ', fg_color: :default, bg_color: :default, style: :default)
-      if ! from.nil?
-        @content = from.content
-        @fg_color = from.fg_color
-        @bg_color = from.bg_color
-        @style = from.style
-      else
-        @content = content
-        @fg_color = fg_color
-        @bg_color = bg_color
-        @style = style
+  class Component < Array
+    attr_accessor :style, :left_pad, :right_pad, :justify
+    attr_reader :fill
+
+    def initialize(line:, content:, style: {}, fill: false, left_pad: 0, right_pad: 0, justify: :left)
+      super()
+      @line = line
+      @style = style
+      @left_pad = left_pad
+      @right_pad = right_pad
+      @fill = fill
+      @justify = justify
+      update(content)
+    end
+
+    def update_style(style)
+      self.map{|x| x.update_style(style)}
+    end
+
+    def update(str)
+      hard = false
+      if self.length == 0
+        hard = true
       end
-      @drawn = false
+      str
+        .to_s
+        .chars
+        .unshift(*Array.new(left_pad) {' '})
+        .push(*Array.new(right_pad) {' '})
+        .each_with_index do |char, i|
+
+        if self[i].nil?
+          self[i] = Character.new(content: char, style: style)
+        else
+          self[i].content = char if (self[i].is_bg || self[i].content != char)
+        end
+      end
+      hard ? line.reset : line.refresh(self)
+    end
+    private
+    attr_reader :line
+  end
+
+  class Cursor
+    attr_reader :position
+    def initialize(line)
+      if ! line.is_a? VP::Line
+        raise VP::VPTypeError, 'cursor MUST have a VP::Line to work with'
+      end
+      @line = line
+      @position = 0
+    end
+
+    def reflow(from: 0, to: nil)
+      to ||= upper_bound
+      line[from, to].each {|c| c.drawn = false}
+      [to, from]
+        .sort {|a,b| ((position - a).abs < (position - b).abs) ? -1 : 1}
+        .each {|target| to(target)}
     end
 
     def draw
-      print format
-      @drawn = true
+      return false if line[position].drawn
+      draw!
     end
 
-    def undraw!
+    def to(target)
+      draw && left # take care of those pesky move-to-current-position calls
+      distance = target - position
+      distance.negative? ? left(distance) : right(distance)
+    end
+
+    def left(qty = 1)
+      qty.abs.times {
+        print "\e[D"
+        @position = position <= 0 ? 0 : position - 1
+        draw && left
+      }
+    end
+
+    def right(qty = 1)
+      qty.abs.times {
+        if ! draw
+          print("\e[C")
+          @position = position >= upper_bound ? upper_bound : position + 1
+        end
+      }
+    end
+
+    def draw!
+      print line[position].format
+      line[position].drawn = true
+      @position = position + 1
+      true
+    end
+
+    private
+    attr_reader :line
+    def off_the_end_check # occurs after writing the last character in the line
+      to(position - 1) if position >= upper_bound
+    end
+
+    def upper_bound
+      line.length - 1
+    end
+  end
+
+  class Character
+    attr_accessor :drawn
+    attr_reader :content, :style, :is_bg
+    DEFAULT_STYLE = {color: :default, background: :default, mode: :default}
+
+    def initialize(content: ' ', style: {}, is_bg: false)
+      @style = DEFAULT_STYLE.merge(style)
       @drawn = false
+      @content = content
+      @is_bg = is_bg
     end
 
     def format
-      content.colorize(color: fg_color, background: bg_color, style: style)
+      content.colorize(**style)
     end
 
-    def blank?
-      content == ' '
+    def style=(new_style)
+      @style = style.merge(new_style)
     end
 
-    def blank!
-      content = ' '
+    def content=(new_char)
+      @content = new_char
       @drawn = false
-    end
-
-    # operators
-    def ==(char)
-      @content == char.content &&
-        @fg_color == char.fg_color &&
-        @bg_color == char.bg_color &&
-        @style == char.style
-    end
-
-    # setters
-    def set(content: nil, fg_color: nil, bg_color: nil, style: nil)
-      @content = content if content
-      @fg_color = fg_color if fg_color
-      @bg_color = bg_color if bg_color
-      @style = style if style
-      @drawn = false
-    end
-
-    def content=(content)
-      @drawn = false
-      @content = content
-    end
-
-    def fg_color=(color)
-      @drawn = false
-      @fg_color = color
-    end
-
-    def bg_color=(color)
-      @drawn = false
-      @bg_color = color
-    end
-
-    def style=(style)
-      @drawn = false
-      @style = style
     end
   end
 end
---
+++ visual-progress.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+# 2.4.2
+require './lib/visual-progress.rb'
+asdf = VP::VisualProgress.new.timestamp
+puts
